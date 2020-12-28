a83121c379018cf0c2208f2330755d4de18f2007
thiagobarros19 <thiago.rck00@gmail.com>
2020-12-28 14:10:32 -0300

    cart service

---
+++ src/app/cart.service.ts
@@ -0,0 +1,59 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+export interface CartProduct {
+  id: number;
+  product: string;
+  image: string;
+  amount: number;
+  price: number;
+  total: number;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CartService {
+
+  private cartProductArray: BehaviorSubject<CartProduct[]> = new BehaviorSubject<CartProduct[]>([]);
+
+  cartProductObs = this.cartProductArray.asObservable();
+
+  insertCartProduct(cartProduct: CartProduct): void{
+    if(cartProduct){
+      let contains = false;
+      let myCartProducts = this.cartProductArray.getValue();
+
+      myCartProducts.map((product, index) => {
+        if(cartProduct.id === product.id){
+          myCartProducts[index].amount++
+          contains = true;
+        }
+      })
+
+      if(!contains) myCartProducts.push(cartProduct);
+
+      this.cartProductArray.next(myCartProducts);
+    }
+  }
+
+  removeCartProduct(cartProductId: number): void{
+    if(cartProductId){
+      let itemIndex = -1;
+      let myCartProducts = this.cartProductArray.getValue();
+
+      myCartProducts.map((product, index) => {
+        if(cartProductId === product.id) itemIndex = index
+      })
+
+      if(itemIndex >= 0) myCartProducts.splice(itemIndex, 1);
+
+      this.cartProductArray.next(myCartProducts);
+    }
+  }
+
+  emptyCartProduct(): void{
+    this.cartProductArray.next([]);
+  }
+
+}
