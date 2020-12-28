c093f43b77fe9ad6267ee8109ef71bf8b1d089a5
thiagobarros19 <thiago.rck00@gmail.com>
2020-12-28 14:09:45 -0300

    Carrinho de compras page att

--- src/app/carrinho/carrinho.component.ts
+++ src/app/carrinho/carrinho.component.ts
@@ -1,4 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { TitleDashboardService } from './../title-dashboard.service';
+import { CartService } from './../cart.service';
+
+export interface CartProduct{
+  id: number;
+  name: string;
+  description: string;
+  image: string;
+  price: number;
+}
 
 @Component({
   selector: 'app-carrinho',
@@ -7,9 +18,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CarrinhoComponent implements OnInit {
 
-  constructor() { }
+  readonly endPoint : string;
+
+  tableColumns: string[] = ['product', 'amount', 'price', 'total'];
+  products : CartProduct[];
+
+  constructor(
+    private http: HttpClient,
+    private titleDashboardService: TitleDashboardService,
+    private cartService: CartService
+  ) {
+    this.endPoint = "http://localhost:8000/api/product/products";
+    this.products = [];
+    titleDashboardService.setPageTitle('Carrinho de compras');
+  }
 
   ngOnInit(): void {
+    this.cartService.cartProductObs.subscribe(product => {
+
+    })
   }
 
 }
