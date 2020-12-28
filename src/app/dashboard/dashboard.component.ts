f9f9325faa20c0b20713a3cde3e669134069a83d
thiagobarros19 <thiago.rck00@gmail.com>
2020-12-28 14:10:02 -0300

    Dashboard page att

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,21 +1,26 @@
 import { Component } from '@angular/core';
 
 import { TitleDashboardService } from './../title-dashboard.service';
+import { CartService } from './../cart.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css'],
-  providers: [TitleDashboardService]
+  providers: [TitleDashboardService, CartService]
 })
 export class DashboardComponent {
 
   title: string;
+  cartAmount: number;
 
   constructor(
-    private titleDashboardService: TitleDashboardService
+    private titleDashboardService: TitleDashboardService,
+    private cartService: CartService
   ) {
     this.title = '';
+    this.cartAmount = 0;
+    cartService.cartProductObs.subscribe( cartProduct => this.cartAmount = cartProduct.length );
     titleDashboardService.pageTitle.subscribe( title => this.title = title );
   }
 
